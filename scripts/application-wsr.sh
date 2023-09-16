9e1226fd419cbb90769055f2b25fdfbebf50f1e9
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-09-16 22:09:37 +0530

    Update application-wsr.sh

--- scripts/application-wsr.sh
+++ scripts/application-wsr.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if [ "wsr" == "main" ]; then
-  cd /var/www/html/wsr-arjit
-  sudo apt-get update
-  # Perform actions for main branch
-fi
+cd /var/www/html/wsr-arjit
+sudo apt-get update
+
