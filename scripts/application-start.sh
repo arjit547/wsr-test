8c6af769e4e5611f87d5b5809cf8a28ca723f636
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-09-16 21:02:34 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$DEPLOYMENT_GROUP_NAME" == "upload" ]; then
+if [ "$wsr" == "upload" ]; then
   cd /var/www/html/wsr-arjit-uni
   sudo apt-get update
   # Perform actions for upload branch
