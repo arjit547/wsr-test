6ea824275c793de128f3240fb1fc4cb1c41b2a98
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-02-16 16:49:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,49 +15,29 @@ ZAP_URL="https://github.com/zaproxy/zaproxy/releases/download/w2024-02-12/ZAP_WE
 # Download OWASP ZAP
 wget "$ZAP_URL" -O zap.zip
 
-# Check if download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download OWASP ZAP. Exiting."
-    exit 1
-fi
+
 
 # Extract OWASP ZAP
 unzip zap.zip -d zap
 
-# Check if extraction was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to extract OWASP ZAP. Exiting."
-    exit 1
-fi
+
 
 # Change directory to the extracted ZAP directory
 cd zap/ZAP_D-2024-02-12
 
 # Run ZAP command
-./zap.sh -cmd -quickurl https://yopmail.com/en -quickprogress -quickout ../zap_report.html
+./zap.sh -cmd -quickurl https://www.example.com -quickprogress -quickout ../zap_report.html
 
-# Check if ZAP command was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to run ZAP command. Exiting."
-    exit 1
-fi
+
 
 # Change directory back to the original location
 cd -
 
-# Check if the report file was generated
-if [ ! -f "zap_report.html" ]; then
-    echo "ZAP report file not found. Exiting."
-    exit 1
-fi
+
 
 # Copy report file to S3 bucket
 aws s3 cp zap_report.html s3://pipelinezapout/
 
-# Check if file copy was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to copy report file to S3 bucket. Exiting."
-    exit 1
-fi
+
 
 echo "ZAP scan and report generation completed successfully."
