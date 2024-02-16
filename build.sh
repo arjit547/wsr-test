cba83760d77ff220b7f0210cb4a4d5b7481e43fa
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-02-16 16:53:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,29 +15,22 @@ ZAP_URL="https://github.com/zaproxy/zaproxy/releases/download/w2024-02-12/ZAP_WE
 # Download OWASP ZAP
 wget "$ZAP_URL" -O zap.zip
 
-
-
 # Extract OWASP ZAP
 unzip zap.zip -d zap
-
-
 
 # Change directory to the extracted ZAP directory
 cd zap/ZAP_D-2024-02-12
 
 # Run ZAP command
-./zap.sh -cmd -quickurl https://www.example.com -quickprogress -quickout ../zap_report.html
+./zap.sh -cmd -quickurl https://www.example.com -quickprogress -quickout zap_report.html
 
-
+# Move the report file to the correct location
+mv zap_report.html ../../
 
 # Change directory back to the original location
 cd -
 
-
-
 # Copy report file to S3 bucket
 aws s3 cp zap_report.html s3://pipelinezapout/
-
-
 
 echo "ZAP scan and report generation completed successfully."
